b7b745b28f80d3d1d63dc64a19c1cd15f5b149be
Vlad <example@gmail.com>
2020-06-12 21:48:43 +0200

    Second commit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+let money = + prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+let firstQuestion = prompt("Введите обязательную статью расходов в этом месяце", "");
+let secondQuestion = prompt("Во сколько обойдется", "");
+
+let appData = {
+    budget : money,
+    timeData : time,
+    expenses : {},
+    optionalExpenses : {
+        firstQuestion:secondQuestion
+    },
+    income: [],
+    savings: false, 
+};
+
+alert(appData.budget/30);
